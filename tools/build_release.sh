7b0ec66eb809872a374f6d4be9d78d61bdd0436c
Leon <me@ljs.dev>
2020-03-22 01:23:07 +1030

    update build script

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -17,8 +17,8 @@ TMP_DIR=$HOME/plugintmp
 rm -Rf $TMP_DIR
 mkdir -p $TMP_DIR
 
-rm -Rf $TMP_DIR/wp2static-addon-s3
-mkdir $TMP_DIR/wp2static-addon-s3
+rm -Rf $TMP_DIR/wp2static-addon-cloudflare-workers
+mkdir $TMP_DIR/wp2static-addon-cloudflare-workers
 
 
 # clear dev dependencies
@@ -28,14 +28,14 @@ composer install --no-dev --optimize-autoloader
 
 
 # cp all required sources to build dir
-cp -r $EXEC_DIR/wp2static-addon-s3.php $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/src $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/assets $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/readme.txt $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/views $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/admin $TMP_DIR/wp2static-addon-s3/
-cp -r $EXEC_DIR/js $TMP_DIR/wp2static-addon-s3/
+cp -r $EXEC_DIR/wp2static-addon-cloudflare-workers.php $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/src $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/assets $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/readme.txt $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/views $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/admin $TMP_DIR/wp2static-addon-cloudflare-workers/
+cp -r $EXEC_DIR/js $TMP_DIR/wp2static-addon-cloudflare-workers/
 
 cd $TMP_DIR
 
@@ -45,10 +45,10 @@ find . -type f -exec chmod 644 {} \;
 
 # strip comments and whitespace from each PHP file
 if [ -z "$2" ]; then
-  find .  ! -name 'wp2static-addon-s3.php' -name \*.php -exec $EXEC_DIR/tools/compress_php_file {} \;
+  find .  ! -name 'wp2static-addon-cloudflare-workers.php' -name \*.php -exec $EXEC_DIR/tools/compress_php_file {} \;
 fi
 
-zip -r -9 ./$1.zip ./wp2static-addon-s3
+zip -r -9 ./$1.zip ./wp2static-addon-cloudflare-workers
 
 cd -
 
