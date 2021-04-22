b89b8d97076d242178c19282e91c8530f4d39def
Leon Stafford <me@ljs.dev>
2021-04-22 15:19:35 +1000

    clean entrypoint file

--- wp2static-addon-cloudflare-workers.php
+++ wp2static-addon-cloudflare-workers.php
@@ -25,33 +25,76 @@ if (! defined('ABSPATH')) {
     die;
 }
 
-// TODO: defines into a Config, like @szepeviktor does in:
-// szepeviktor/small-project/blob/346d4c3008a2fc67f47df19a5b54c9546a27f135/plugin-name.php#L75
-define('WP2STATIC_CLOUDFLARE_WORKERS_PATH', plugin_dir_path(__FILE__));
-define('WP2STATIC_CLOUDFLARE_WORKERS_VERSION', '1.0-dev');
+// Load autoloader.
+if (! class_exists(Config::class) && is_file(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
 
+// Prevent double activation.
+if (Config::get('version') !== null) {
+    add_action(
+        'admin_notices',
+        static function (): void {
+            // phpcs:ignore Squiz.PHP.DiscouragedFunctions.Discouraged
+            error_log(
+                'WP2Static Cloudflare Workers Add-on double activation. ' .
+                'Please remove all but one copies. ' . __FILE__
+            );
+
+            if (! current_user_can('activate_plugins')) {
+                return;
+            }
+
+            printf(
+                '<div class="notice notice-warning"><p>%1$s<br>%2$s&nbsp;<code>%3$s</code></p></div>',
+                esc_html__(
+                    'WP2Static Cloudflare Workers Add-on already installed! Please deactivate all but one copies.',
+                    'wp2static-addon-cloudflare-workers'
+                ),
+                esc_html__('Current plugin path:', 'wp2static-addon-cloudflare-workers'),
+                esc_html(__FILE__)
+            );
+        },
+        0,
+        0
+    );
+    return;
+}
+
+// Define constant values.
+Config::init(
+    [
+        'version' => '0.0.1',
+        'filePath' => __FILE__,
+        'baseName' => plugin_basename(__FILE__),
+        'slug' => 'wp2static-addon-cloudflare-workers',
+    ]
+);
+
+// Check requirements.
 if (
-    file_exists(WP2STATIC_CLOUDFLARE_WORKERS_PATH . 'vendor/autoload.php')
+    (new Requirements())
+        ->php('7.4')
+        ->wp('5.3')
+        ->multisite(false)
+        ->met()
 ) {
-    require WP2STATIC_CLOUDFLARE_WORKERS_PATH . 'vendor/autoload.php';
+    // Hook plugin activation functions.
+    register_activation_hook(__FILE__, __NAMESPACE__ . '\\activate');
+    register_deactivation_hook(__FILE__, __NAMESPACE__ . '\\deactivate');
+    register_uninstall_hook(__FILE__, __NAMESPACE__ . '\\uninstall');
+    add_action('plugins_loaded', __NAMESPACE__ . '\\boot', 10, 0);
+
+    // Support WP-CLI.
+    if (defined('WP_CLI') && \WP_CLI === true) {
+        registerCliCommands();
+    }
+} else {
+    // Suppress "Plugin activated." notice.
+    unset($_GET['activate']); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+
+    add_action('admin_notices', __NAMESPACE__ . '\\printRequirementsNotice', 0, 0);
+
+    require_once \ABSPATH . 'wp-admin/includes/plugin.php';
+    deactivate_plugins([(string)Config::get('baseName')], true);
 }
-
-// TODO: don't run global fn, rather, do req's check and load/fail:
-// szepeviktor/small-project/blob/346d4c3008a2fc67f47df19a5b54c9546a27f135/plugin-name.php#L92
-function runWp2staticAddonCloudflareWorkers()
-{
-    $controller = new WP2StaticCloudflareWorkers\Controller();
-    $controller->run();
-}
-
-register_activation_hook(
-    __FILE__,
-    [ 'WP2StaticCloudflareWorkers\Controller', 'activate' ]
-);
-
-register_deactivation_hook(
-    __FILE__,
-    [ 'WP2StaticCloudflareWorkers\Controller', 'deactivate' ]
-);
-
-runWp2staticAddonCloudflareWorkers();
